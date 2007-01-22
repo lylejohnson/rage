2d82a2402a9623d1a06a12e67900aa16d0934ba2
lyle <lyle@7b727165-1dff-0310-b919-82edcc8b1397>
2007-01-22 22:16:13 +0000

    Reduced the list of services for the DF and AMS agents to one each (i.e. the fipa-df service and fipa-ams service). We don't need to have a separate service description for each of the kinds of messages that these agents can respond to (register, unregister, search, modify).
    git-svn-id: file:///Users/lyle/Repository/rage/trunk@72 7b727165-1dff-0310-b919-82edcc8b1397

--- lib/rage/ams_agent.rb
+++ lib/rage/ams_agent.rb
@@ -3,40 +3,15 @@ module RAGE
   class AMSAgent < RAGE::Agent
 
     def register_capabilities
-      register_service_description = RAGE::ServiceDescription.new(
-        :name => "Agent Registration",
-        :protocols => [ "fipa-request" ],
-        :ontologies => [ "fipa-agent-management" ]
-      )
-      deregister_service_description = RAGE::ServiceDescription.new(
-        :name => "Agent Deregistration",
-        :protocols => [ "fipa-request" ],
-        :ontologies => [ "fipa-agent-management" ]
-      )
-      modify_service_description = RAGE::ServiceDescription.new(
-        :name => "Agent Modification",
-        :protocols => [ "fipa-request" ],
-        :ontologies => [ "fipa-agent-management" ]
-      )
-      search_service_description = RAGE::ServiceDescription.new(
-        :name => "Agent Search",
-        :protocols => [ "fipa-request" ],
-        :ontologies => [ "fipa-agent-management" ]
-      )
-      get_description_service_description = RAGE::ServiceDescription.new(
-        :name => "Platform Description",
+      service_description = RAGE::ServiceDescription.new(
+        :name => "Agent Management System",
+        :type => "fipa-ams",
         :protocols => [ "fipa-request" ],
         :ontologies => [ "fipa-agent-management" ]
       )
       agent_description = RAGE::DFAgentDescription.new(
         :name => aid,
-        :services => [
-          register_service_description,
-          deregister_service_description,
-          modify_service_description,
-          search_service_description,
-          get_description_service_description
-        ]
+        :services => [ service_description ]
       )
       df.register(agent_description)
     end
--- lib/rage/df_agent.rb
+++ lib/rage/df_agent.rb
@@ -3,34 +3,15 @@ module RAGE
   class DFAgent < RAGE::Agent
 
     def register_capabilities
-      register_service_description = RAGE::ServiceDescription.new(
-        :name => "Service Registration",
-        :protocols => [ "fipa-request" ],
-        :ontologies => [ "fipa-agent-management" ]
-      )
-      deregister_service_description = RAGE::ServiceDescription.new(
-        :name => "Service Deregistration",
-        :protocols => [ "fipa-request" ],
-        :ontologies => [ "fipa-agent-management" ]
-      )
-      modify_service_description = RAGE::ServiceDescription.new(
-        :name => "Service Modification",
-        :protocols => [ "fipa-request" ],
-        :ontologies => [ "fipa-agent-management" ]
-      )
-      search_service_description = RAGE::ServiceDescription.new(
-        :name => "Service Search",
+      service_description = RAGE::ServiceDescription.new(
+        :name => "Directory Facilitator",
+        :type => "fipa-df",
         :protocols => [ "fipa-request" ],
         :ontologies => [ "fipa-agent-management" ]
       )
       agent_description = RAGE::DFAgentDescription.new(
         :name => aid,
-        :services => [
-          register_service_description,
-          deregister_service_description,
-          modify_service_description,
-          search_service_description
-        ]
+        :services => [ service_description ],
       )
       df.register(agent_description)
     end
