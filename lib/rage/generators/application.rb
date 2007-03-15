24cb2e5e0740a2f78c553e25bf122d7f738e1bef
lyle <lyle@7b727165-1dff-0310-b919-82edcc8b1397>
2007-03-15 20:59:13 +0000

    git-svn-id: file:///Users/lyle/Repository/rage/trunk@86 7b727165-1dff-0310-b919-82edcc8b1397

--- lib/rage/generators/application.rb
+++ lib/rage/generators/application.rb
@@ -4,17 +4,25 @@ module RAGE
 
   module Generators
 
+    #
+    # Generates the directory structure for a RAGE application.
+    #
     class ApplicationGenerator
       
-      def initialize(args)
+      #
+      # Run the application generator with the given array
+      # of command-line arguments.
+      #
+      def run(args)
         app_path = ARGV.first
         FileUtils.mkdir app_path
         FileUtils.touch File.join(app_path, "Rakefile") # FIXME
+        FileUtils.touch File.join(app_path, "README") # FIXME
         FileUtils.mkdir File.join(app_path, "log")
         FileUtils.mkdir File.join(app_path, "script")
       end
       
-    end # Application
+    end # ApplicationGenerator
 
   end # end Generators
 
